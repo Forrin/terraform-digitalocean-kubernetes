54f5df539199c36f90622eb6004e59e03c218003
Forrin <Forrin@users.noreply.github.com>
2022-06-06 20:41:13 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "digitalocean_kubernetes_cluster" "this" {
+  name    = var.cluster_name
+  region  = var.region
+  version = var.cluster_version
+
+  auto_upgrade = var.auto_upgrade
+
+  node_pool {
+    name = var.default_node_pool.name
+    size = var.default_node_pool.size
+    node_count = var.default_node_pool.node_count
+  }
+}
+
+resource "digitalocean_kubernetes_node_pool" "this" {
+  for_each = var.node_pools
+
+  cluster_id = digitalocean_kubernetes_cluster.this.id
+
+  name       = each.value.name
+  size       = each.value.size
+  node_count = each.value.node_count
+}
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "cluster_name" {
+  type = string
+  description = "K8s Cluster Name."
+}
+
+variable "region" {
+  type = string
+  description = "K8s Cluster Region."
+}
+
+variable "cluster_version" {
+  type = string
+  description = "K8s Cluster Version."
+  default = "1.22.8-do.1"
+}
+
+variable "auto_upgrade" {
+  type = bool
+  description = "Enable auto upgrade during maintenance window."
+  default = true
+}
+
+variable "surge_upgrade" {
+  type = bool
+  description = "Enable surge upgrade during maintenance window."
+  default = false
+}
+
+variable "default_node_pool" {
+  type = object({
+    name = string
+    size = string
+    node_count = number
+  })
+  
+  description = "Cluster default node pool."
+}
+
+variable "node_pools" {
+  type = map(
+    object({
+      name = string
+      size = string
+      node_count = number
+    })
+  )
+
+  description = "Cluster additional node pools."
+  default = {}
+}
